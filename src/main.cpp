6ea14995fdc4ee071db419ebed5538969899155f
taobaocoindev <1002725164@qq.com>
2014-11-04 14:01:22 +0800

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2806,7 +2806,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("0x73a09bb41a4037af39b7fdcf25f0fa853148a51c68915bb0fc025b1d899b2de3"));
         block.print();
         assert(hash == hashGenesisBlock);
 
